4c0a692fe171de06c5b65a73dac32e91c377fa67
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-10-23 09:21:59 +0300

    initial project setup; resolve ambiguities using fully qualified notation

---
+++ NamespacesExamples/Program.cs
@@ -0,0 +1,22 @@
+﻿//using NamespacesExamples.TwoD;
+//using NamespacesExamples.ThreeD;
+
+namespace NamespacesExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoD.Point p2D = new TwoD.Point();
+            p2D.X = 100;
+            p2D.Y = 200;
+
+            ThreeD.Point p3D = new ThreeD.Point();
+            p3D.X = 100;
+            p3D.Y = 200;
+            p3D.Z = 300;
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
